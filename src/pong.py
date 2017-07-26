f44d25d0453a67a681c58e07c6dae33074677c6c
Adam Saladino <adam@codingsimply.com>
2017-07-26 06:39:23 -0400

    Moved pong into a class Settings dialog now updates and saves (sort of).

--- src/pong.py
+++ src/pong.py
@@ -7,50 +7,70 @@ from models.ball import Ball
 from repositories.settings_repository import SettingsRepository
 from ui.settings_dialog import SettingsDialog
 
-settingsRepository = SettingsRepository('settings.bin')
 
-setting = settingsRepository.read()
-settingsDialog = SettingsDialog()
+class Pong:
 
-pygame.init()
-pygame.display.set_caption('Pong')
+    def __init__(self):
+        self.settingsRepository = SettingsRepository('settings.bin')
+        self.setting = self.settingsRepository.read()
+        self.settingsDialog = SettingsDialog(self.setting)
 
-board = Board((setting.boardWidth, setting.boardHeight))
-score = Score(board)
-ball = Ball(board)
-paddle1 = Paddle(board)
-paddle2 = Paddle(board, left=False)
+        pygame.init()
+        pygame.display.set_caption('Pong')
 
-userPlayer1Controller = setting.get_controller1(paddle1)
-userPlayer2Controller = setting.get_controller2(paddle2)
+        self.board = Board((self.setting.boardWidth, self.setting.boardHeight))
+        self.score = Score(self.board)
+        self.ball = Ball(self.board)
+        self.paddle1 = Paddle(self.board)
+        self.paddle2 = Paddle(self.board, left=False)
 
-while True:
-    for event in pygame.event.get():
-        score.did_reset(event)
-        board.game_did_end(event)
-        ball.did_restart(event)
-        userPlayer1Controller.did_paddle_move(event, ball)
-        userPlayer2Controller.did_paddle_move(event, ball)
-        settingsDialog.should_display(event)
+        self.userPlayer1Controller = self.setting.get_controller1(self.paddle1)
+        self.userPlayer2Controller = self.setting.get_controller2(self.paddle2)
 
-    userPlayer1Controller.did_paddle_move_alone(ball)
-    userPlayer2Controller.did_paddle_move_alone(ball)
+        self.gameOn = False
 
-    ball.did_hit(paddle1)
-    ball.did_hit(paddle2)
+    def game_on(self):
+        self.gameOn = True
+        self.game_loop()
 
-    score.player1 += paddle2.did_miss(ball)
-    score.player2 += paddle1.did_miss(ball)
+    def game_off(self):
+        self.gameOn = False
 
-    board.render()
-    score.render()
-    ball.render()
-    paddle1.render()
-    paddle2.render()
+    def game_loop(self):
+        while self.gameOn:
+            self.event_check()
 
-    pygame.display.flip()
+            self.userPlayer1Controller.did_paddle_move_alone(self.ball)
+            self.userPlayer2Controller.did_paddle_move_alone(self.ball)
 
-    image_data = pygame.surfarray.array3d(pygame.display.get_surface())
+            self.ball.did_hit(self.paddle1)
+            self.ball.did_hit(self.paddle2)
 
-    userPlayer1Controller.learn(image_data, score)
-    userPlayer2Controller.learn(image_data, score)
+            self.score.player1 += self.paddle2.did_miss(self.ball)
+            self.score.player2 += self.paddle1.did_miss(self.ball)
+
+            self.board.render()
+            self.score.render()
+            self.ball.render()
+            self.paddle1.render()
+            self.paddle2.render()
+
+            pygame.display.flip()
+
+            image_data = pygame.surfarray.array3d(pygame.display.get_surface())
+
+            self.userPlayer1Controller.learn(image_data, self.score)
+            self.userPlayer2Controller.learn(image_data, self.score)
+
+    def event_check(self):
+        for event in pygame.event.get():
+            self.score.did_reset(event)
+            self.board.game_did_end(event)
+            self.ball.did_restart(event)
+            self.userPlayer1Controller.did_paddle_move(event, self.ball)
+            self.userPlayer2Controller.did_paddle_move(event, self.ball)
+            self.settingsDialog.should_display(event, self.setting)
+
+
+pong = Pong()
+pong.game_on()
--- src/ui/settings_dialog.py
+++ src/ui/settings_dialog.py
@@ -4,20 +4,18 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVB
     QGridLayout, QComboBox, QTabWidget
 import pygame
 
-
 # http://zetcode.com/gui/pyqt5/firstprograms/
 from models.setting import ControllerTypes
 
 
 class SettingsDialog:
-    def __init__(self):
+    def __init__(self, setting):
         self.app = QApplication(sys.argv)
         self.window = QWidget()
         self.window.resize(250, 150)
         self.window.setWindowTitle('Settings')
-        self.build_ui()
+        self.setting = setting
 
-    def build_ui(self):
         tabs = QTabWidget()
         options_tab = QWidget()
         controller1_tab = QWidget()
@@ -34,17 +32,18 @@ class SettingsDialog:
         controllers_player1_label = QLabel("Controllers")
         controllers_player2_label = QLabel("Controllers")
 
-        board_height_edit = QLineEdit()
-        board_width_edit = QLineEdit()
+        self.board_height_edit = QLineEdit()
+        self.board_width_edit = QLineEdit()
 
-        controllers_player1_combo = QComboBox()
-        controllers_player2_combo = QComboBox()
+        self.controllers_player1_combo = QComboBox()
+        self.controllers_player2_combo = QComboBox()
 
         for controller in ControllerTypes:
-            controllers_player1_combo.addItem(controller.value)
-            controllers_player2_combo.addItem(controller.value)
+            self.controllers_player1_combo.addItem(controller.value)
+            self.controllers_player2_combo.addItem(controller.value)
 
-        ok_button = QPushButton("OK")
+        ok_button = QPushButton("Save")
+        ok_button.clicked.connect(self.save)
         cancel_button = QPushButton("Cancel")
         cancel_button.clicked.connect(self.window.hide)
 
@@ -53,10 +52,10 @@ class SettingsDialog:
         options_grid.setSpacing(10)
 
         options_grid.addWidget(board_height_label, 1, 0)
-        options_grid.addWidget(board_height_edit, 1, 1)
+        options_grid.addWidget(self.board_height_edit, 1, 1)
 
         options_grid.addWidget(board_width_label, 2, 0)
-        options_grid.addWidget(board_width_edit, 2, 1)
+        options_grid.addWidget(self.board_width_edit, 2, 1)
 
         options_v_box = QVBoxLayout()
         options_v_box.addStretch(1)
@@ -68,7 +67,7 @@ class SettingsDialog:
         controller1_grid.setSpacing(10)
 
         controller1_grid.addWidget(controllers_player1_label, 1, 0)
-        controller1_grid.addWidget(controllers_player1_combo, 1, 1)
+        controller1_grid.addWidget(self.controllers_player1_combo, 1, 1)
 
         controller1_v_box = QVBoxLayout()
         controller1_v_box.addStretch(1)
@@ -80,7 +79,7 @@ class SettingsDialog:
         controller2_grid.setSpacing(10)
 
         controller2_grid.addWidget(controllers_player2_label, 1, 0)
-        controller2_grid.addWidget(controllers_player2_combo, 1, 1)
+        controller2_grid.addWidget(self.controllers_player2_combo, 1, 1)
 
         controller2_v_box = QVBoxLayout()
         controller2_v_box.addStretch(1)
@@ -100,6 +99,34 @@ class SettingsDialog:
 
         self.window.setLayout(v_box)
 
-    def should_display(self, event):
+    def should_display(self, event, setting):
         if event.type == pygame.KEYDOWN and event.key == pygame.K_s:
+            self.setting = setting
+            self.update_setting()
             self.window.show()
+
+    def update_setting(self):
+        self.board_height_edit.setText(str(self.setting.boardHeight))
+        self.board_width_edit.setText(str(self.setting.boardWidth))
+        self.controllers_player1_combo.setCurrentText(self.setting.controller1.value)
+        self.controllers_player2_combo.setCurrentText(self.setting.controller2.value)
+
+    def save(self):
+        self.setting.boardHeight = int(self.board_height_edit.text())
+        self.setting.boardWidth = int(self.board_width_edit.text())
+
+        if self.controllers_player1_combo.currentText() is ControllerTypes.USER:
+            self.setting.controller1 = ControllerTypes.USER
+        if self.controllers_player1_combo.currentText() is ControllerTypes.LEARNING_AI:
+            self.setting.controller1 = ControllerTypes.LEARNING_AI
+        if self.controllers_player1_combo.currentText() is ControllerTypes.LOGICAL_AI:
+            self.setting.controller1 = ControllerTypes.LOGICAL_AI
+
+        if self.controllers_player2_combo.currentText() is ControllerTypes.USER:
+            self.setting.controller2 = ControllerTypes.USER
+        if self.controllers_player2_combo.currentText() is ControllerTypes.LEARNING_AI:
+            self.setting.controller2 = ControllerTypes.LEARNING_AI
+        if self.controllers_player2_combo.currentText() is ControllerTypes.LOGICAL_AI:
+            self.setting.controller2 = ControllerTypes.LOGICAL_AI
+
+        self.window.hide()
